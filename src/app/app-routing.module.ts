dd3edceed60c6d3cf698ddf6b6294a623cafc455
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-06-22 18:11:55 -0500

    Componentes y paquetes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientEditComponent } from './components/client-edit/client-edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingComponent } from './components/setting/setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: "", component: tablero},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "setting", component: SettingComponent},
+  {path: "client/:id", component: ClientEditComponent},
+  {path: "**", component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { ClientEditComponent } from './components/client-edit/client-edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ClientsComponent,
+    SettingComponent,
+    ClientEditComponent,
+    LoginComponent,
+    NotFoundComponent,
+    FooterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
